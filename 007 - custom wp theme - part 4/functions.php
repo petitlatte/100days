e7279fcc2efc275bb1b34ede5fe0ef02da7ce702
petitlatte <lisasumida@gmail.com>
2020-02-07 16:33:26 +1300

    Day 7

---
+++ 007 - custom wp theme - part 4/functions.php
@@ -0,0 +1,42 @@
+<?php 
+
+//Widget
+register_sidebar(); 
+
+//RSS Feed
+add_theme_support('automatic-feed-links');
+
+//Custom Menu
+register_nav_menu('navigation', 'Navigation');
+
+//Custom Header
+add_theme_support('custom-header', array(
+	'width' => 1500,
+	'height' => 250,
+	'default-image' => '%s/test.jpg',
+	'header-text' => false
+	) );
+
+//excerpt Word limit 
+function my_length($length) {
+	return 25;
+}
+
+add_filter('excerpt_mblength','my_length');
+
+function my_more($more) {
+	return '...';
+}
+
+add_filter('excerpt_more','my_more');
+
+//Custom background
+add_theme_support( 'custom-background' );
+
+//thumbnail
+add_theme_support( 'post-thumbnails' );
+
+
+
+
+?>
