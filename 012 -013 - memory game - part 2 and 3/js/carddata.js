6e03ebf6963f1f2b248f8fdb12d897defd9bd24c
petitlatte <lisasumida@gmail.com>
2020-02-13 13:49:53 +1300

    Day 13 start over

--- 012 -013 - memory game - part 2 and 3/js/carddata.js
+++ 012 -013 - memory game - part 2 and 3/js/carddata.js
@@ -1,51 +1,35 @@
 const cardsArray = [
   {
     name: 'Morepork',
-    img: '../img/morepork.png',
+    img: 'img/morepork.png',
   },
   {
     name: 'Kereru',
-    img: '../img/kereru.png',
+    img: 'img/kereru.png',
   },
   {
     name: 'Kakapo',
-    img: '../img/kakapo.png',
+    img: 'img/kakapo.png',
   },
   {
     name: 'Tui',
-    img: '../img/tui.png',
+    img: 'img/tui.png',
   },
   {
     name: 'Whio',
-    img: '../img/whio.png',
+    img: 'img/whio.png',
   },
   {
     name: 'Kiwi',
-    img: '../img/kiwi.png',
-  },
-  {
-    name: 'Parakeet',
-    img: '../img/parakeet.png',
+    img: 'img/kiwi.png',
   },
   {
     name: 'Fantail',
-    img: '../img/fantail.png',
+    img: 'img/fantail.png',
   },
   {
     name: 'Kokako',
-    img: '../img/kokako.png',
+    img: 'img/kokako.png',
   },
-  {
-    name: 'Silvereye',
-    img: '../img/silvereye.png',
-  },
-  {
-    name: 'Kingfisher',
-    img: '../img/kingfisher.png',
-  },
-  {
-    name: 'NZ Falcon',
-    img: '../img/nzfalcon.png',
-  },  
 
-]
+  ]
